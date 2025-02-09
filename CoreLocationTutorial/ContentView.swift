882615cedfa587bab8d9492680455663e27c8df3
James Stigall <james_stgll@yahoo.com>
2025-02-08 22:39:25 -0500

    Initial Commit

---
+++ CoreLocationTutorial/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  CoreLocationTutorial
+//
+//  Created by James Stigall on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationDataManager = LocationDataManager()
+    
+    
+    var body: some View {
+        /*Text("Hello, world!")
+            .padding()*/
+        VStack {
+                    switch locationDataManager.locationManager.authorizationStatus {
+                    case .authorizedWhenInUse:  // Location services are available.
+                        // Insert code here of what should happen when Location services are authorized
+                        Text("Your current location is:")
+                        Text("Latitude: \(locationDataManager.locationManager.location?.coordinate.latitude.description ?? "Error loading")")
+                        Text("Longitude: \(locationDataManager.locationManager.location?.coordinate.longitude.description ?? "Error loading")")
+                        
+                    case .restricted, .denied:  // Location services currently unavailable.
+                        // Insert code here of what should happen when Location services are NOT authorized
+                        Text("Current location data was restricted or denied.")
+                    case .notDetermined:        // Authorization not determined yet.
+                        Text("Finding your location...")
+                        ProgressView()
+                    default:
+                        ProgressView()
+                    }
+                }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreLocationTutorial/CoreLocationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationTutorialApp.swift
+//  CoreLocationTutorial
+//
+//  Created by James Stigall on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
